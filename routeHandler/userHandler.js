cacb86822ab98239465f505129ba2b22263bd07c
aktarulrahul <contact.aktarul@gmail.com>
2021-09-21 20:25:32 +0600

    login handler added

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const router = express.Router();
 const userSchema = require('../schemas/userSchema');
@@ -23,6 +24,47 @@ router.post('/signup', async (req, res) => {
   } catch {
     res.status(500).json({
       error: 'Signup failed',
+    });
+  }
+});
+// Login
+router.post('/signin', async (req, res) => {
+  try {
+    const user = await User.find({ username: req.body.username });
+    if (user && user.length > 0) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user[0].password
+      );
+      if (isValidPassword) {
+        // Generate token
+        const token = jwt.sign(
+          {
+            username: user[0].username,
+            userId: user[0]._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: '1h',
+          }
+        );
+        res.status(200).json({
+          access_token: token,
+          message: 'Login successfully',
+        });
+      } else {
+        res.status(401).json({
+          error: 'Authetication failed',
+        });
+      }
+    } else {
+      res.status(401).json({
+        error: 'Authetication failed',
+      });
+    }
+  } catch {
+    res.status(401).json({
+      error: 'Authetication failed',
     });
   }
 });
